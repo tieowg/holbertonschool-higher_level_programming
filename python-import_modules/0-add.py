d6d1df3d05da9ac3edf20705f2bad52c714e29a4
tieowg <tieowg@example.com>
2025-06-17 09:34:08 +0000

    firstcommit

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-def add(a, b):
-    return a+b
-print(add(1,2))
+from add_0 import add
+
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
