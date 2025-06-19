a2dbdab3bbd2baf83f1bca39957564888993d4df
tieowg <tieowg@example.com>
2025-06-19 09:31:13 +0000

    L

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    id idx < 0 or
+    if idx < 0 or idx > len(my_list):
+        return my_list
+    else:
+        return my_list(idx, new_element)
