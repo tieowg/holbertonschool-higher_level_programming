58a2dfa2b7bd8a8787c772e77e216e8d284459da
tieowg <tieowg@example.com>
2025-06-19 08:51:46 +0000

    LL

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,5 +2,7 @@
 def element_at(my_list, idx):
     if idx < 0:
         return None
+    element = my_list[idx]
     else:
-        print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
+        print("Element at index {:d} is {}".format(idx, element))
+    return element
