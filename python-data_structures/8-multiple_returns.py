b0c13f927cb44358b9ab47d174809fa9f81262d8
tieowg <tieowg@example.com>
2025-06-20 10:51:23 +0000

    Salam

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentecne) == 0:
-        return None 
-    else:
-        print(len(sentence), sentence[0])
+        return (0, None) 
+    return (len(sentence), sentence[0])
