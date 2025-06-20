448610be0de6d0fa669a634c159933b7176b6ce4
tieowg <tieowg@example.com>
2025-06-20 13:03:17 +0000

    Salam

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -2,10 +2,11 @@
 def delete_at(my_list=[], idx=0):
     if idx >= len(my_list) or idx < 0:
         return my_list.copy()
-    newlist = []
-    for i in range(len(my_list)):
-        if i == idx:
-            continue
-        else:
-            newlist.append(my_list[i])
-    return newlist
+    else:
+        newlist = []
+        for i in range(len(my_list)):
+            if i == idx:
+                continue
+            else:
+                newlist.append(my_list[i])
+        return newlist
