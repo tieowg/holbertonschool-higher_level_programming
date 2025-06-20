1bfda5eb28ba21d0807b4bca368847c70a7262dc
tieowg <tieowg@example.com>
2025-06-20 12:49:46 +0000

    Salam

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -2,4 +2,4 @@
 def divisible_by_2(my_list=[]):
     new_list = []
     for i in range(len(my_list)):
-        print("is", if my_list[i] % 2 == 0 else "is not" )
+        print("is" if my_list[i] % 2 == 0 else "is not" )
