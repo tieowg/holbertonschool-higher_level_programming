98ce491654b4429179afe6d919b6ff8c3d9bdff0
tieowg <tieowg@example.com>
2025-06-26 09:46:45 +0000

    L

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -8,7 +8,7 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-        number_of_instances += 1
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -52,4 +52,4 @@ class Rectangle:
 
     def __del__(self):
         print("Bye rectangle...")
-        number_of_instances -= 1
+        Rectangle.number_of_instances -= 1
