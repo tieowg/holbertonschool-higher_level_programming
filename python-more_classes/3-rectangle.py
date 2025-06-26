c5533a549085dc8cf69fc6acad455f9aeaa116c4
tieowg <tieowg@example.com>
2025-06-26 08:43:46 +0000

    L

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -43,4 +43,4 @@ class Rectangle:
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
             return ""
-        return return "\n".join(["#" * self.__width for _ in range(self.__height)])
+        return "\n".join(["#" * self.__width for _ in range(self.__height)])
