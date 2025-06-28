7fa270f28381f0e19a15c3f163afe3dd5bb7f75b
tieowg <tieowg@example.com>
2025-06-28 11:44:05 +0000

    n

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''salam'''
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+'''salam'''
+
+
+class Rectangle(BaseGeometry):
+    '''salam'''
+    def __init__(self, width, height):
+        self.integer_validator('width', width)
+        self.integer_validator('height', height)
+        self.__width = width
+        self.__height = height
+
+
+if __name__ == "__main__":
+    print(issubclass(Rectangle, BaseGeometry))
